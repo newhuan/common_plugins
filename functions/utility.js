332513f41aa0626af9a0b3a808bc3d451d4f8134
newhuan <newhuan2017@gmail.com>
2017-07-14 19:07:00 +0800

    add functions isNull isUndefined reverseString

--- functions/utility.js
+++ functions/utility.js
@@ -10,10 +10,31 @@ function isUndefined(parm){
     return typeof parm === 'undefined';
 }
 
-if (typeof DEBUG !== "undefined") {
-    console.log( "Debugging is starting" );
+//reverse a string
+function reverseString(string) {
+    if(typeof string !== 'string'){
+        return false
+    }
+
+    // Step 1: deal with combining marks and astral symbols (surrogate pairs)
+    string = string
+    // Swap symbols with their combining marks so the combining marks go first
+        .replace(/(<%= allExceptCombiningMarks %>)(<%= combiningMarks %>+)/g, function($0, $1, $2) {
+            // Reverse the combining marks so they will end up in the same order
+            // later on (after another round of reversing)
+            return reverseString($2) + $1;
+        })
+        // Swap high and low surrogates so the low surrogates go first
+        .replace(/([\uD800-\uDBFF])([\uDC00-\uDFFF])/g, '$2$1');
+    // Step 2: reverse the code units in the string
+    var result = '';
+    var index = string.length;
+    while (index--) {
+        result += string.charAt(index);
+    }
+    return result;
 }
-"a".logd
+
 /************************************/
 /************************************/
 /************************************/
